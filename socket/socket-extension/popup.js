5c87b6f17582ac036abed10199d785564924cb43
sehgol <>
2020-03-02 23:15:11 -0500

    updates

--- socket/socket-extension/popup.js
+++ socket/socket-extension/popup.js
@@ -1,25 +1,52 @@
-// var socket = io();
 
-//         $('form').submit(function(){
+    var port = 3201;
+    var socket = io.connect('159.203.70.158:' + port);
+io.on('connection', function (socket) {
 
-//           socket.emit('message', $('#txt').val());
+    // var socket = io();
+    console.log(socket)
 
-//           $('#txt').val('');
+    $('form').submit(function(){
 
-//           return false;
+      socket.emit('message', $('#txt').val());
 
-//         });
-var port = 3201;
+      $('#txt').val('');
 
-var socket = io.connect('159.203.70.158:' + port);
+      return false;
+
+    });
+
+    socket.on('message', function(msg){
+
+      $('#message').append($('<li>').text(msg));
+
+    });
+
+  });
 
 
-$('form').submit(function(e){
-    
-    console.log(`event is ${event}`)
-    e.preventDefault(); 
-    var text = $('#txt').val();
-    console.log(`text is ${text}`)
-    socket.emit('message', text);
-});
+// // var socket = io();
+
+// //         $('form').submit(function(){
+
+// //           socket.emit('message', $('#txt').val());
+
+// //           $('#txt').val('');
+
+// //           return false;
+
+// //         });
+// var port = 3201;
+
+// var socket = io.connect('159.203.70.158:' + port);
+
+
+// $('form').submit(function(e){
+
+//     console.log(`event is ${event}`)
+//     e.preventDefault(); 
+//     var text = $('#txt').val();
+//     console.log(`text is ${text}`)
+//     socket.emit('message', text);
+// });
 
